b19219acf15670ce13f23ebd3628cd0ea4e371a6
Ekoé Jean <70731505+JeanKouss@users.noreply.github.com>
2022-06-26 23:11:10 +0000

    Début index

---
+++ assets/js/global.js
@@ -0,0 +1,33 @@
+
+/* Animation à l'apparition */
+let elmsToObserve = document.querySelectorAll(".anime-on-apparition");
+
+let onIntersectClassAdd = "isVisibleOnScreen";
+let ratio = 0.1;
+let options = {
+    root : null,
+    rootMargin : '0px',
+    threshold : 1.0
+}
+
+// Fonction de callback
+const handleRootIntersect = function(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add(onIntersectClassAdd);
+            rootObserver.unobserve(entry.target);
+        }
+    });
+}
+// Création de l'observeur
+const rootObserver = new IntersectionObserver(handleRootIntersect, options);
+
+if (rootObserver) { // Affectation de l'observeur s'il a été bien créé
+    for (let elm of elmsToObserve) {
+        rootObserver.observe(elm);
+    }
+} else { // Si le observer n'est pas supporté
+    for (let elm of elmsToObserve) {
+        elm.classList.add(onIntersectClassAdd);
+    }
+}
